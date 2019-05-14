83970a9747ce22fa7b056ba0a1266005efdaf507
Heidi <hengstrom25@gmail.com>
2019-05-13 22:27:49 -0400

    create intial recipeReducer

---
+++ recipe-box/client/src/reducers/recipesReducer.js
@@ -0,0 +1,20 @@
+export default (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_RECIPE': {
+			return state.concat(action.recipe);
+		}	
+			
+		case 'REMOVE_RECIPE':{
+			const index = state.findIndex(recipe => recipe.id === action.recipeId);
+			return [...state.slice(0, index), ...state.slice(index + 1)]
+		}
+		
+		case 'SET_RECIPES': {
+			return action.recipes;
+		}
+		
+		default: {
+			return state;
+		}
+	}
+}
