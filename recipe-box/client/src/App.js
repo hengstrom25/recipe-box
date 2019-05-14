940cd9703bb45739d972f20e93a362bf58d37ce2
Heidi <hengstrom25@gmail.com>
2019-05-13 20:17:23 -0400

    add ids to practice data

--- recipe-box/client/src/App.js
+++ recipe-box/client/src/App.js
@@ -1,6 +1,6 @@
 //import React from 'react';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import CategoryCard from './components/CategoryCard';
@@ -8,21 +8,21 @@ import RecipeBox from './components/RecipeBox';
 import RecipeCard from './components/RecipeCard';
 
 const data = {categories: [
-  {name: "Salads",
+  {name: "Salads", id: 0,
    recipes: [
-      {name: "Mom's Tossed Salad",
+      {name: "Mom's Tossed Salad", id: 0,
        url: "http://greatcooks.com/blah/123",
        comments: "This is a good salad"
       },
-     {name: "Dad's Potato Salad",
+     {name: "Dad's Potato Salad", id: 1,
       url: "http://goodstuff.com/abc/456",
       comments: "Many calories"
      }
    ]
   },
-  {name: "Desserts",
+  {name: "Desserts", id: 1,
     recipes: [
-      {name: "Chocolate Cake",
+      {name: "Chocolate Cake", id: 0,
        url: "http://easybake.com/xyz/999",
        comments: "Love it"
       }
@@ -34,11 +34,14 @@ const data = {categories: [
 class App extends Component {
 	render() {
 		return (
+		<Router>
 			<div>
+				<Route exact path='/recipebox' component={RecipeBox}/>
 				<RecipeBox categories={data.categories}/>
 				<CategoryCard recipes={data.categories[0].recipes}/>
 				<RecipeCard recipe={data.categories[0].recipes[0]}/>
 			</div>
+		</Router>
 		)
 	}
 };
