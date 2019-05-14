34a1c4cd19afc977f0084f321cc694bf3ca1c274
Heidi <hengstrom25@gmail.com>
2019-05-13 22:37:25 -0400

    create store.js

---
+++ recipe-box/client/src/store.js
@@ -0,0 +1,11 @@
+import [ createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import recipes from './reducers/recipesReducer';
+
+const reducers = combineReducers({ recipes ]);
+
+export default createStore(
+	reducers, 
+	window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_(),
+	applyMiddleware(thunk)
+)
