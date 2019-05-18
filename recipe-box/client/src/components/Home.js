8e18de51015781c7f1c9012210f2cbbc3774f804
Heidi <hengstrom25@gmail.com>
2019-05-18 11:15:35 -0400

    add open link to home page

--- recipe-box/client/src/components/Home.js
+++ recipe-box/client/src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Link} from 'react-router-dom';
 
  const Home = () => {
   return (
     <div className ='App'>
 		<header className='App-header'>
 			<h1 className='App-title'>Recipe Box</h1>
+			<h2>
+				<Link to="/recipebox">Open</Link>
+			</h2>
 		</header>
     </div>
   );
