b895d1e8807bec2fab5b5ca9869bb4e06c3207c7
Heidi <hengstrom25@gmail.com>
2019-06-01 18:47:37 -0400

    form progress

--- recipe-box/client/src/components/NewRecipe.js
+++ recipe-box/client/src/components/NewRecipe.js
@@ -4,6 +4,16 @@ import RecipeForm from './RecipeForm'
 class NewRecipe extends React.Component {
 	submit = values => {
 		console.log(values)
+		
+		const data = new URLSearchParams();
+		data.append("name", values.name);
+		data.append("recipe_field", values.recipe_field);
+		data.append("notes", values.notes);
+		
+		return fetch("http://localhost:3001/recipes", {
+			method: "POST",
+			body: data,
+		})
 	}
 	render() {
 		return <RecipeForm onSubmit={this.submit}/>
