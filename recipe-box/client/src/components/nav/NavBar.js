b1db0c47fc76f6455570a53d002f31a884728ca0
Heidi <hengstrom25@gmail.com>
2019-06-04 20:53:52 -0400

    navbar progress before making more simple

--- recipe-box/client/src/components/nav/NavBar.js
+++ recipe-box/client/src/components/nav/NavBar.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import store from '../../store.js';
+import { fetchCategories } from '../../actions/categories';
 import './navbar.css';
 
+const mapStateToProps = state => 
+	({
+		categories: state.categories.allIds.map(id => state.categories.byId[id])
+	})
 
-const NavBar = () => {
-	return (
-		<div className='navbar'>
-			<Link to='/recipebox'>Recipe Box</Link>
-				<ul>
-					<li><Link to='/category/1'>Breakfast</Link></li>
-					<li><Link to='/category/2'>Salad</Link></li>
-					<li><Link to='/category/3'>Soup</Link></li>
-					<li><Link to='/category/4'>Side Dishes</Link></li>
-					<li><Link to='/category/5'>Meat Dishes</Link></li>
-					<li><Link to='/category/6'>Vegetarian</Link></li>
-					<li><Link to='/category/7'>Pasta</Link></li>
-					<li><Link to='/category/8'>Dessert</Link></li>
-					<li><Link to='/category/9'>Other</Link></li>
-				</ul>
-			</div>
+class NavBarPresentation extends Component { 
+	constructor(props) {
+	super(props)
+	}
+	
+	render() {
+	
+		const { categories } = this.props;
+		
+		return (
+			<ul>
+				{categories.map(category => (
+					<li key={category.id}>
+						<Link to={`/category/${category.id}`}>{category.name}</Link>
+					</li>
+				))}
+			</ul>
 		)
-	};
+	}
+	
+	componentDidMount() {
+		store.dispatch(fetchCategories())
+		console.log('component did mount')
+	}
+
+};
+
+const NavBar = connect(mapStateToProps, null)(NavBarPresentation);
 	
 export default NavBar;				
