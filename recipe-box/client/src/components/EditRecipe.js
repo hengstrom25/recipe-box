c6b983e67d974cadcb3074bcedc03a75286e82a1
Heidi <hengstrom25@gmail.com>
2019-06-08 22:23:27 -0400

    work on edit form - trying for solution to fill form with state

--- recipe-box/client/src/components/EditRecipe.js
+++ recipe-box/client/src/components/EditRecipe.js
@@ -2,11 +2,16 @@ import React from 'react';
 import RecipeForm from './RecipeForm'
 /*import { initialize } from 'redux-form'*/
 import { connect } from 'react-redux'
+import store from '../store.js';
+import { fetchRecipe } from '../actions/recipes';
+import { fetchCategories } from '../actions/categories';
 
 const mapStateToProps = (state, ownProps) => 
-	{const recipe=state.recipes.byId[ownProps.id] || {name: "", recipe_field: "", notes: "", category_id: 0}
+	{const recipe=state.recipes.byId[parseInt(ownProps.id)] || {name: "", recipe_field: "", notes: "", category_id: 0, id:parseInt(ownProps.id)}
+	console.log(state, ownProps)
 		return {
 		recipe: recipe,
+		id: parseInt(ownProps.id),
 		category: state.categories.byId[recipe.category_id] || {name: ""}
 	}}
 
@@ -23,7 +28,7 @@ class EditRecipeForm extends React.Component {
 		data.append("notes", values.notes);
         data.append("category_id", this.props.category_id);
 		
-		return fetch("http://localhost:3001/recipe/" + this.props.recipe.id, {
+		return fetch("http://localhost:3001/recipe/" + this.props.id, {
 			method: "PATCH",
 			body: data,
 		}).then(response => response.json())
@@ -40,6 +45,11 @@ class EditRecipeForm extends React.Component {
 	render() {
 		return <RecipeForm onSubmit={this.submit} recipe={this.props.recipe}/>
 	}
+	
+	componentDidMount() {
+	 	store.dispatch(fetchRecipe(this.props.id)) 
+	 	store.dispatch(fetchCategories())
+	}	
 }
 
 const EditRecipe = connect(mapStateToProps, null)(EditRecipeForm);
