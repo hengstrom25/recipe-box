97a41fb78bc6b41251dcb9bfcf13617b193baa24
Heidi <hengstrom25@gmail.com>
2019-06-17 19:58:09 -0400

    update recipe box, simplify and delete container

---
+++ recipe-box/client/src/components/RecipeBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting'
+
+const RecipeBoxPresentation = () => (
+
+	<div>
+		<Greeting />
+	</div>
+			
+);
+
+export default RecipeBoxPresentation
